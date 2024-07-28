598a09c094bc88ccd9f404b4fa922597ae472a91
abdoulfataoh <abdoulfataoh@gmail.com>
2024-07-28 20:46:30 +0200

    implement db user model

---
+++ fastapi_oauth2_service/models/user.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# Copyright (C) 2024 vela
+
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    Column,
+    String,
+    Boolean,
+)
+
+from fastapi_oauth2_service.models.base import BaseModelMixin
+
+
+class User(BaseModelMixin):
+    __tablename__ = 'auth_users'
+    username = Column(String(64), unique=True, nullable=False)
+    email = Column(String, unique=True, nullable=True)
+    full_name = Column(String(128), nullable=True)
+    phone_number = Column(String(32), unique=True, nullable=True)
+    disabled = Column(Boolean, default=False)
+    hashed_password = Column(String, nullable=False)
+    authorizations = relationship('Authorization', back_populates='user')
