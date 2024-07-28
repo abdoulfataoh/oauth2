f81c3f58dd4b4228b025afca3c50760cfdfd9fa4
abdoulfataoh <abdoulfataoh@gmail.com>
2024-07-28 21:34:07 +0200

    export user and client schemas

---
+++ fastapi_oauth2_service/schemas/__init__.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from fastapi_oauth2_service.schemas.client import ClientBase, ClientCreate, Client
+from fastapi_oauth2_service.schemas.user import UserBase, UserCreate, User
+
+
+__all__ = [
+    'ClientBase',
+    'ClientCreate',
+    'Client',
+    'UserBase',
+    'UserCreate',
+    'User',
+]
