ed4063c1fc8e127fdbd9ee973db18b36c8b9f6b0
abdoulfataoh <abdoulfataoh@gmail.com>
2024-07-29 18:35:53 +0200

    fix missing self for Secret class Methods

--- fastapi_oauth2_service/security/encryption.py
+++ fastapi_oauth2_service/security/encryption.py
@@ -8,10 +8,10 @@ class Secret:
     def __init__(self) -> None:
         pass
 
-    def hash(secret: str) -> str:
+    def hash(self, secret: str) -> str:
         hash = bcrypt.hash(secret)
         return hash
 
-    def verify(secret: str, hash: str) -> bool:
+    def verify(self, secret: str, hash: str) -> bool:
         status = bcrypt.verify(secret, hash)
         return status
