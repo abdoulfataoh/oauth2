bf384b72ece0cbb80a22392426ad934c23430ea8
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-15 15:48:40 +0100

    implement crud funtions

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,31 +1,22 @@
 # coding: utf-8
 
 from app.crud.user import (
-    create_user,
-    update_user,
-    get_user,
-    get_user_by_username,
-    get_users,
-    delete_user,
+    create_user, update_user, get_user,
+    get_user_by_username, get_users, delete_user,
 )
-from app.crud.client import get_client
+from app.crud.client import (
+    create_client, get_client, delete_client, get_clients
+)
 from app.crud.authorization import (
-    create_authorization,
-    update_authorization,
-    delete_authorization,
+    create_authorization, update_authorization, delete_authorization,
     get_authorizations_by_user_id,
 )
 
 __all__ = [
-    'create_user',
-    'update_user',
-    'get_user',
-    'get_user_by_username',
-    'get_users',
-    'delete_user',
-    'get_client',
+    'create_user', 'update_user', 'get_user',
+    'get_user_by_username', 'get_users', 'delete_user',
+    'create_client', 'get_client', 'delete_client',  'get_clients',
+    'create_authorization', 'update_authorization',
+    'delete_authorization', 'get_authorizations_by_user_id',
     'create_authorization',
-    'update_authorization',
-    'delete_authorization',
-    'get_authorizations_by_user_id',
 ]
--- app/crud/client.py
+++ app/crud/client.py
@@ -1,19 +1,72 @@
 # coding: utf-8
 
+from typing import Sequence
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
-from app.models import Client as ClientModel
-from app.traces.function_trace import trace_call
+from app import schemas as S
+from app import models as M
+from app.utils.log import trace
+from app.utils.security import Secret
 
 
 __all__ = [
-    'get_client',
+    'create_client', 'get_client', 'delete_client',
+    'get_clients',
 ]
 
 
-@trace_call
-async def get_client(db: AsyncSession, client_id: str) -> ClientModel | None:
-    result = await db.execute(select(ClientModel).where(ClientModel.client_id == client_id))
+@trace
+async def create_client(db: AsyncSession, client: S.ClientCreate) -> M.Client:
+    """
+    Create a new client application
+    """
+    client_secret = Secret.generate_secret(16)
+    client_secret_hash = Secret.hash(client_secret)
+
+    db_client = M.Client(
+        client_name=client.client_name,
+        redirect_uri=client.redirect_uri,
+        client_id=Secret.generate_secret(16),
+        client_secret=client_secret_hash
+    )
+    db.add(db_client)
+    await db.commit()
+    await db.refresh(db_client)
+    db_client.client_secret = client_secret
+    return db_client
+
+
+@trace
+async def get_clients(db: AsyncSession, skip: int = 0, limit: int = 10) -> Sequence[M.Client]:
+    """
+    Retrieve all clients informations
+    """
+    result = await db.execute(select(M.Client).offset(skip).limit(limit))
+    db_clients = result.scalars().all()
+    return db_clients
+
+
+@trace
+async def get_client(db: AsyncSession, client_id: str) -> M.Client | None:
+    """
+    Retrieve an existing client informations by client_id
+    """
+    result = await db.execute(select(M.Client).where(M.Client.client_id == client_id))
     db_client = result.scalars().first()
     return db_client
+
+
+@trace
+async def delete_client(db: AsyncSession, client_id: int) -> M.Client | None:
+    """
+    Delete an existing client by client_id
+    """
+    result = await db.execute(select(M.Client).where(M.Client.client_id == client_id))
+    db_client = result.scalars().first()
+    if db_client:
+        await db.delete(db_client)
+        await db.commit()
+        return db_client
+    return None
