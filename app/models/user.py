d87c24092fd4b8e04ba69950d525dcf27749db8b
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-15 10:46:59 +0100

    define firstname and lastname instead of full_name

--- app/models/user.py
+++ app/models/user.py
@@ -25,7 +25,8 @@ class User(BaseModelMixin):
     __tablename__ = 'auth_users'
     username = Column(String(64), unique=True, nullable=False)
     email = Column(String, unique=True, nullable=True)
-    full_name = Column(String(128), nullable=True)
+    firstname = Column(String(128), nullable=True)
+    lastname = Column(String(128), nullable=True)
     phone_number = Column(String(32), unique=True, nullable=True)
     disabled = Column(Boolean, default=False)
     hashed_password = Column(String, nullable=False)
