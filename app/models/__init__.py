0e7fe6231544811d7f9316420d9b5e16947c1de8
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-15 20:39:26 +0100

    define Session model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,10 +3,12 @@
 from app.models.client import Client
 from app.models.user import User
 from app.models.authorization import Authorization
+from app.models.session import Session
 
 
 __all__ = [
     'Client',
     'User',
     'Authorization',
+    'Session',
 ]
