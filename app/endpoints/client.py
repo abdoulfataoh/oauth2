8b43558976b12dc47fc867dd0f06263d22746fcb
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-15 16:03:18 +0100

    separate endpoints

---
+++ app/endpoints/client.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from fastapi import Depends
+from fastapi import APIRouter
+
+from app.utils.exceptions import (
+    ClientNotFoundException, ConflictedClientException
+)
+from app import schemas as S
+from app import crud as CRUD
+from app.db import get_db
+
+
+__all__ = [
+    'router',
+]
+
+
+router = APIRouter()
+
+
+@router.post('/oauth2/client', response_model=S.Client,)
+async def add_client(client: S.ClientCreate, db: AsyncSession = Depends(get_db)) -> S.Client:
+    try:
+        db_client = await CRUD.create_client(db=db, client=client)
+        client_data = S.Client.model_validate(db_client)
+    except IntegrityError:
+        raise ConflictedClientException
+    return client_data
+
+
+@router.get('/oauth2/clients', response_model=list[S.Client])
+async def get_clients(db: AsyncSession = Depends(get_db)) -> list[S.Client]:
+    db_clients = await CRUD.get_clients(db=db)
+    clients_data = [S.Client.model_validate(db_client) for db_client in db_clients]
+    return clients_data
+
+
+@router.get('/oauth2/client/{client_id}', response_model=S.Client)
+async def get_client(client_id: str, db: AsyncSession = Depends(get_db)) -> S.Client:
+    db_client = await CRUD.get_client(db=db, client_id=client_id)
+    if not db_client:
+        raise ClientNotFoundException
+    client_data = S.Client.model_validate(db_client)
+    return client_data
+
+
+@router.delete('/oauth2/client/{client_id}')
+async def delete_client(client_id: str, db: AsyncSession = Depends(get_db)) -> S.Client:
+    db_client = await CRUD.delete_client(db=db, client_id=client_id)
+    if not db_client:
+        raise ClientNotFoundException
+    client_data = S.Client.model_validate(db_client)
+    return client_data
---
+++ app/endpoints/user.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from fastapi import Depends
+from fastapi import APIRouter
+
+from app.utils.exceptions import (
+    ConflictedUserException, UserNotFoundException,
+)
+from app import schemas as S
+from app import crud as CRUD
+from app.db import get_db
+
+
+__all__ = [
+    'router',
+]
+
+
+router = APIRouter()
+
+
+@router.post('/oauth2/user', response_model=S.User,)
+async def add_user(user: S.UserCreate, db: AsyncSession = Depends(get_db)) -> S.User:
+    try:
+        db_user = await CRUD.create_user(db=db, user=user)
+        user_data = S.User.model_validate(db_user)
+    except IntegrityError:
+        raise ConflictedUserException
+    return user_data
+
+
+@router.get('/oauth2/users', response_model=list[S.User])
+async def get_users(db: AsyncSession = Depends(get_db)) -> list[S.User]:
+    db_users = await CRUD.get_users(db=db)
+    users_data = [S.User.model_validate(db_user) for db_user in db_users]
+    return users_data
+
+
+@router.get('/oauth2/user/{username}', response_model=S.User)
+async def get_user_by_username(username: str, db: AsyncSession = Depends(get_db)) -> S.User:
+    db_user = await CRUD.get_user_by_username(db=db, username=username)
+    if not db_user:
+        raise UserNotFoundException
+    user_data = S.User.model_validate(db_user)
+    return user_data
+
+
+@router.delete('/oauth2/user/{user_id}')
+async def delete_user(user_id: str, db: AsyncSession = Depends(get_db)) -> S.User:
+    db_user = await CRUD.delete_user(db=db, user_id=user_id)
+    if not db_user:
+        raise UserNotFoundException
+    user_data = S.User.model_validate(db_user)
+    return user_data
