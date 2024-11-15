321367b390d6cca115a4272e71b264cc18cfd998
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-15 16:01:11 +0100

    update routes

--- start.py
+++ start.py
@@ -1,9 +1,18 @@
 # coding: utf-8
 
 from fastapi import FastAPI
+from fastapi.exceptions import RequestValidationError
 
-from app.api import router
+from app.endpoints import health, user, client
+from app.utils.exceptions_handlers import validation_exception_handler
 
 
 app = FastAPI()
-app.include_router(router, tags=['Authentification'])
+
+app.add_exception_handler(RequestValidationError, validation_exception_handler)
+
+app.include_router(health.router, tags=['Monitoring'])
+
+app.include_router(user.router, tags=['User'])
+
+app.include_router(client.router, tags=['Client Application'])
