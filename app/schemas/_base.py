9144d8fd6a7aebc7359c5c9b845f3bdf91d4beca
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-15 10:55:02 +0100

    implement TimestampMixin

--- app/schemas/_base.py
+++ app/schemas/_base.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 from uuid import uuid4
+from datetime import datetime
 
 from pydantic import BaseModel
 from pydantic import Field
@@ -13,3 +14,8 @@ __all__ = [
 
 class IdMixin(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid4()))
+
+
+class TimestampMixin(BaseModel):
+    created_at: datetime = Field(default_factory=datetime.now)
+    edited_at: datetime | None = None
