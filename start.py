c6c35bb4c7d863bc869a0551ab1f17d6f88a595a
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-17 00:51:35 +0100

    raname login to auth

--- start.py
+++ start.py
@@ -3,7 +3,7 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 
-from app.endpoints import health, login, user, client
+from app.endpoints import health, auth, user, client
 from app.utils.exceptions_handlers import validation_exception_handler
 
 
@@ -13,7 +13,7 @@ app.add_exception_handler(RequestValidationError, validation_exception_handler)
 
 app.include_router(health.router, tags=['Monitoring'])
 
-app.include_router(login.router, tags=['Login'])
+app.include_router(auth.router, tags=['Auth'])
 
 app.include_router(user.router, tags=['User'])
 
