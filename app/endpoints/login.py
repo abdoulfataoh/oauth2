dad5156ccf34c0dc6a22deab96c0c187ee6e0c15
abdoulfataoh <abdoulfataoh@gmail.com>
2024-11-17 00:45:03 +0100

    prevent crsf attack

--- app/endpoints/login.py
+++ app/endpoints/login.py
@@ -1,11 +1,10 @@
 # coding: utf-8
 
-from datetime import timedelta
 from typing import Annotated
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from fastapi import APIRouter, Form, Depends, Response
-# from fastapi.responses import RedirectResponse
+from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app import settings
 from app.utils.exceptions import LoginFailedException
@@ -23,37 +22,21 @@ __all__ = [
 router = APIRouter()
 
 
-@router.post('/oauth2/login/')
+@router.post('/oauth2/token')
 async def login(
-    username: Annotated[str, Form()], password: Annotated[str, Form()],
-    response: Response, db: AsyncSession = Depends(get_db),
+    login_form: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: AsyncSession = Depends(get_db),
 ) -> S.Token:
+    """
+    Login for token
+    """
     # Auth user
     db_user = await CRUD.authenticate_user(
-        db=db, username=username, password=password
+        db=db, username=login_form.username, password=login_form.password
     )
     if not db_user:
         raise LoginFailedException
 
-    # Write session
-    db_session = await CRUD.create_session(db=db, username=username)
-    if not db_session:
-        raise LoginFailedException
-
-    # # Set cookie
-    # session_id = db_session.session_id
-    # response.set_cookie(key='session_id', value=session_id, max_age=timedelta(minutes=30))
-
     token_expire = settings.ACCESS_TOKEN_EXPIRE_MINUTES
     token = create_jwt(user_id=db_user.id, expires_in=token_expire)
-    print(db_user.id)
-
-    response.set_cookie(
-        key='access_token', 
-        value=token, 
-        max_age=timedelta(minutes=token_expire), 
-        httponly=True,
-        secure=True,
-        samesite='Strict'
-    )
-    return {f'{username}': f'{token}'}
+    return S.Token(access_token=token, token_type='bearer')
--- app/helpers/_security.py
+++ app/helpers/_security.py
@@ -1,7 +1,11 @@
 # coding: utf-8
 
-from fastapi import Request, Depends
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.ext.asyncio import AsyncSession
+
 
 from app import schemas as S
 from app.utils.security import decode_jwt
@@ -10,14 +14,17 @@ from app.db import get_db
 from app.utils.exceptions import UserNotFoundException
 
 
-async def _check_user(request: Request, db: AsyncSession = Depends(get_db)) -> S.User | None:
-    access_token = request.cookies.get('access_token')
-    if not access_token:
-        """
-        NotTokenFound, redirect ?
-        """
-    payload = decode_jwt(access_token)
-    print(payload)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/oauth2/token')
+
+
+async def check_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: AsyncSession = Depends(get_db)
+) -> S.User | None:
+    """
+    Decode user data by token
+    """
+    payload = decode_jwt(token)
     user_id = payload['sub']
     db_user = await CRUD.get_user(db=db, user_id=user_id)
     if not db_user:
