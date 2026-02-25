dc5361845279c2466abf13c73e6028f9f400b499
abdoulfataoh <abdoulfataoh@gmail.com>
2026-02-25 13:05:40 +0100

    Update settings vars

--- app/settings.py
+++ app/settings.py
@@ -4,60 +4,64 @@ import logging
 
 from environs import Env
 
-
 env = Env()
 env.read_env()
 
-# Vars
+# [ VARS ]
 MINUTE = 60
 HOUR = MINUTE * 60
+DAY = HOUR * 24
 
-# [ Trace ]
-TEST = env.bool('TEST', default=True)
+
+# [ TRACE ]
+TEST = env.bool('TEST', default=False)
 DEBUG = TEST
+LOGLEVEL = logging.DEBUG if DEBUG else logging.INFO
+logging.basicConfig(level=LOGLEVEL, format='%(levelname)s - %(asctime)s - %(message)s')
 
-if TEST:
-    LOG_LEVEL = logging.DEBUG
-else:
-    LOG_LEVEL = logging.INFO
 
-logging.basicConfig(level=LOG_LEVEL, format='%(levelname)s - %(asctime)s - %(message)s')
+# [ DATABASE ]
+DATABASE_TYPE = env.str('DATABASE_TYPE', default='sqlite3')
+DATABASE_SERVER = env.str('DATABASE_SERVER', default='')
+DATABASE_USERNAME = env.str('DATABASE_USER', default='')
+DATABASE_PASSWORD = env.str('DATABASE_PASSWORD', default='')
+DATABASE_NAME = env.str('DATABASE_NAME', default='')
 
-# [ Database ]
-DATABASES = {
-    'default': {
+DATABASES_URL_TEMPLATES = {
+    'sqlite3': {
         'url': 'sqlite+aiosqlite:///db.sqlite3',
     },
 
     'mysql': {
-        'url': 'mysql://{user}:{password}@{server}/{db}'
+        'url': 'mysql://{username}:{password}@{server}/{database}'
     },
 
     'postgresql': {
-        'url': 'postgresql://{user}:{password}@{server}/{db}'
+        'url': 'postgresql://{username}:{password}@{server}/{database}'
     }
 }
 
-DATABASE_USER = env.str('DATABASE_USER', default='')
-DATABASE_PASSWORD = env.str('DATABASE_PASSWORD', default='')
-DATABASE_SERVER = env.str('DATABASE_SERVER', default='')
-DATABASE_NAME = env.str('DATABASE_NAME', default='')
-
-DB = 'default' if TEST else 'mysql'
-
-_DATABASE_URL_ = DATABASES[DB]['url'].format(
-    user=DATABASE_USER,
-    password=DATABASE_PASSWORD,
-    server=DATABASE_SERVER,
-    db=DATABASE_NAME
+DATABASE_URL = DATABASES_URL_TEMPLATES[DATABASE_TYPE]['url'].format(
+    database=DATABASE_NAME, server=DATABASE_SERVER,
+    username=DATABASE_USERNAME, password=DATABASE_PASSWORD,
 )
-DATABASE_URL = env.str('DATABASE_URL', default=_DATABASE_URL_)
 
-# [ Secret ]
-API_PREFIX = env.str('API_PREFIX', default='oauth')
-SECRET_KEY = env.str('SECRET_KEY', default='1234')
+
+# [JWT]
+JWT_SECRET_KEY = env.str('JWT_SECRET_KEY', default='1234')
 JWT_ALGORITHM = env.str('JWT_ALGORITHM', default='HS256')
-ACCESS_TOKEN_EXPIRE_MINUTES = env.int('ACCESS_TOKEN_EXPIRE_MINUTES', default=60*60)
 
-# [ General ]
-TIMEZONE = env.str('TIMEZONE', default='UTC')
+
+# [OAUTH BACKEND]
+OAUTH_API_PREFIX = env.str('OAUTH_API_PREFIX', default='/oauth2')
+OAUTH_API_URI = env.str('OAUTH_API_URI', default='localhost:8000')
+
+# [TOKEN & EXPIRE TIME]
+UI_ACCESS_TOKEN_EXPIRE_MINUTES = env.int('ACCESS_TOKEN_EXPIRE_MINUTES', default=3*HOUR)
+AUTHORIZATION_CODE_EXPIRE_MINUTES = env.int('AUTHORIZATION_CODE_EXPIRE_MINUTES', default=5*DAY)
+REQUEST_AUTHORIZATION_EXPIRE_TIME = env.int('REQUEST_EXPIRE_TIME', default=3*MINUTE)
+
+# [OAUTH UI]
+OAUTH_UI_URI = env.str('OAUTH_UI_URI', default='http://localhost:5173')
+OAUTH_UI_LOGIN_URL = f'{OAUTH_UI_URI}/login'
+OAUTH_UI_CONSENT_URL = f'{OAUTH_UI_URI}/consent'
