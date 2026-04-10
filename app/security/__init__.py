f2d45248870e93f21e9b820d144dbb2935b3a9cf
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-10 16:17:34 +0200

    Update package export

--- app/security/__init__.py
+++ app/security/__init__.py
@@ -1,1 +1,11 @@
 # coding: utf-8
+
+from app.security.dependencies import CurrentUser, CurrentOptionalUser
+from app.security.permissions import AdminUser
+
+
+__all__ = [
+    'CurrentUser',
+    'CurrentOptionalUser',
+    'AdminUser',
+]
