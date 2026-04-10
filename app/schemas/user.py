7f6b6b18a53a115345b3039fe338e914810f0c44
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-10 15:58:52 +0200

    Update user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,19 +1,33 @@
 # coding: utf-8
 
+from datetime import date
+
 from pydantic import BaseModel, ConfigDict
-from pydantic import Field, SecretStr, EmailStr
+from pydantic import Field, SecretStr, EmailStr, model_validator
 
 from app.schemas.base import BaseORM
 
 
 class UserCommon(BaseModel):
-    email: EmailStr
-    phone: str | None = Field(min_length=8, max_length=32)
+
+    email: EmailStr | None = None
+    phone: str | None = Field(
+        default=None,
+        min_length=8,
+        max_length=32
+    )
+
+    @model_validator(mode='after')
+    def validate_contact(self):
+        if not self.email and not self.phone:
+            raise ValueError('email or phone required')
+        return self
 
 
 class UserBase(BaseModel):
     firstname: str
-    lastname: str | None = None
+    lastname: str
+    birthdate: date | None = None
 
 
 class UserCreate(UserBase, UserCommon):
@@ -24,15 +38,19 @@ class UserInfoUpdate(UserBase):
     pass
 
 
+class UserPasswordUpdate(BaseModel):
+    old_password: str
+    new_password: str
+
+
 class UserEmailUpdate(BaseModel):
     email: EmailStr
 
 
 class User(BaseORM, UserBase, UserCommon):
-    """
-    User in DB representation
-    """
     model_config = ConfigDict(from_attributes=True)
+
     username: str
     roles: list[str] | str = 'user'
-    disabled: bool | None = None
+    verified: bool
+    disabled: bool
