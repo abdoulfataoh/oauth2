6c308a8319795225e97c32d6abfd629bf385444a
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-10 15:50:50 +0200

    Update package export

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,10 +3,14 @@
 from app.models.client import Client
 from app.models.user import User
 from app.models.oauth import OAuthAuthorizationRequest, OAuthAuthorizationCode
+from app.models.otp import Otp
+from app.models.session import UserSession
 
 __all__ = [
     'Client',
     'User',
     'OAuthAuthorizationRequest',
     'OAuthAuthorizationCode',
+    'Otp',
+    'UserSession',
 ]
