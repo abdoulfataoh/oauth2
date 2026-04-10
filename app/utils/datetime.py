29d3a562967c2c5c2ae14c8223cdb6449920c6f7
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-10 15:27:56 +0200

    Define expired datetime checker

--- app/utils/datetime.py
+++ app/utils/datetime.py
@@ -8,3 +8,14 @@ def utcnow() -> datetime:
     Return current UTC datetime
     """
     return datetime.now(tz=timezone.utc)
+
+
+def ensure_utc(dt):
+    if dt.tzinfo is None:
+        return dt.replace(tzinfo=timezone.utc)
+    return dt
+
+
+def is_expired(dt):
+    dt = ensure_utc(dt)
+    return dt < utcnow()
