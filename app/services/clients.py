7574c8a8263ddcba3855001cf4716ba8589b33c1
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-10 21:05:46 +0200

    Migrate fastapi exception to domi

--- app/services/clients.py
+++ app/services/clients.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from uuid import uuid4
+from uuid import uuid4, UUID
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
@@ -9,10 +9,9 @@ from app import crud
 from app import schemas as S
 from app import models as M
 from app.utils.security import generate_secret, hash_password
-from app.utils.exceptions import (
-    ConflictedClientException,
-    InvalidFieldsException,
-    ClientNotFoundException,
+
+from app.exceptions.domain import (
+    ClientAlreadyExistsError, ClientNotFoundError, InvalidFieldsError,
 )
 
 
@@ -20,26 +19,31 @@ async def create_client_service(
     db: AsyncSession,
     client: S.ClientCreate,
 ) -> tuple[M.Client, str]:
-    client_id = str(uuid4().hex).upper()
+
+    client_id = uuid4().hex.upper()
+
     client_secret = generate_secret(32)
     client_secret_hash = hash_password(client_secret)
 
     try:
         db_client = await crud.create_client(
-            db,
+            db=db,
             client_name=client.client_name,
             redirect_uri=client.redirect_uri,
             client_id=client_id,
             client_secret_hash=client_secret_hash,
             allowed_scopes=client.allowed_scopes,
         )
+
     except IntegrityError as e:
         msg = str(e.orig).lower()
 
         if 'unique' in msg:
-            raise ConflictedClientException()
+            raise ClientAlreadyExistsError()
+
         if 'not null' in msg:
-            raise InvalidFieldsException("Missing required field")
+            raise InvalidFieldsError("Missing required field")
+
         raise
 
     return db_client, client_secret
@@ -50,40 +54,59 @@ async def get_clients_service(
     skip: int = 0,
     limit: int = 10,
 ) -> list[M.Client]:
-    return list(await crud.get_clients(db, skip, limit))
+
+    clients = await crud.get_clients(
+        db=db,
+        skip=skip,
+        limit=limit,
+    )
+
+    return list(clients)
 
 
 async def get_client_by_client_id_service(
     db: AsyncSession,
     client_id: str,
 ) -> M.Client:
-    db_client = await crud.get_client_by_client_id(db, client_id)
+
+    db_client = await crud.get_client_by_client_id(
+        db=db,
+        client_id=client_id,
+    )
 
     if not db_client:
-        raise ClientNotFoundException
+        raise ClientNotFoundError()
 
     return db_client
 
 
 async def get_client_by_id_service(
     db: AsyncSession,
-    client_id: str,
+    client_id: UUID,
 ) -> M.Client:
-    db_client = await crud.get_client_by_id(db, client_id)
+
+    db_client = await crud.get_client_by_id(
+        db=db,
+        client_id=client_id,
+    )
 
     if not db_client:
-        raise ClientNotFoundException
+        raise ClientNotFoundError()
 
     return db_client
 
 
 async def delete_client_by_id_service(
     db: AsyncSession,
-    client_id: str,
+    client_id: UUID,
 ) -> M.Client:
-    db_client = await crud.delete_client_by_id(db, client_id)
+
+    db_client = await crud.delete_client_by_id(
+        db=db,
+        client_id=client_id,
+    )
 
     if not db_client:
-        raise ClientNotFoundException
+        raise ClientNotFoundError()
 
     return db_client
