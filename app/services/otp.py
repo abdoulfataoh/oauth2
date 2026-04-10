de6598ae9af4798a44b797789c6d357238713d64
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-10 21:44:29 +0200

    Manage secure action with OTP

---
+++ app/services/otp.py
@@ -0,0 +1,237 @@
+# coding: utf-8
+
+from uuid import UUID
+from datetime import timedelta
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app import settings, crud
+from app import models as M
+
+from app.utils.security import generate_otp
+from app.utils.datetime import utcnow, is_expired
+from app.utils.security import hash_password
+
+from app.exceptions.domain import (
+    OtpExpiredError,
+    TooManyVerificationAttemptsError,
+    InvalidOtpError,
+    UserNotFoundError,
+)
+
+
+def resolve_otp_recipient(user):
+    channel = 'email' if user.email else ('sms' if user.phone else None)
+    recipient = user.email or user.phone
+    return channel, recipient
+
+
+async def send_otp_service(
+    db: AsyncSession,
+    *,
+    user_id: UUID,
+    recipient: str,
+    otp_type: str,
+    channel: str,
+) -> str:
+
+    code = generate_otp()
+    expires_at = utcnow() + timedelta(minutes=settings.OTP_EXPIRE_MINUTES)
+
+    existing_otp = await crud.get_otp(
+        db,
+        user_id=user_id,
+        recipient=recipient,
+        otp_type=otp_type,
+        channel=channel,
+    )
+
+    if existing_otp:
+        await crud.delete_otp(db, existing_otp.id)
+
+    await crud.create_otp(
+        db,
+        user_id=user_id,
+        recipient=recipient,
+        channel=channel,
+        code=code,
+        otp_type=otp_type,
+        expires_at=expires_at,
+    )
+
+    print('\n\n\n\n\n\n\n')
+    print('*****************************')
+    print('***********', code, '********')
+    print('***********', otp_type, '********')
+    print('***********', channel, '********')
+    print('***********', recipient, '********')
+    print('*****************************')
+    print('\n\n\n\n\n\n\n')
+
+    return code
+
+
+async def _validate_otp(
+    db: AsyncSession,
+    *,
+    user_id: UUID,
+    recipient: str,
+    otp_type: str,
+    channel: str,
+    code: str,
+    consume: bool = True
+
+) -> M.Otp:
+
+    db_otp = await crud.get_otp(
+        db,
+        user_id=user_id,
+        recipient=recipient,
+        otp_type=otp_type,
+        channel=channel,
+    )
+
+    if not db_otp:
+        raise InvalidOtpError("Invalid OTP")
+
+    if is_expired(db_otp.expires_at):
+        raise OtpExpiredError("OTP expired")
+
+    if db_otp.attempts >= settings.MAX_ATTEMPTS:
+        raise TooManyVerificationAttemptsError("Too many attempts")
+
+    if db_otp.code != code:
+        await crud.increment_otp_attempts(db, otp_id=db_otp.id)
+        raise InvalidOtpError("Invalid OTP")
+
+    if consume:
+        await crud.delete_otp(db, otp_id=db_otp.id)
+
+    return db_otp
+
+
+async def validate_otp_without_consume(
+    db: AsyncSession,
+    *,
+    user_id: UUID | None,
+    recipient: str,
+    otp_type: str,
+    channel: str,
+    code: str,
+) -> M.Otp:
+
+    if not user_id:
+        raise InvalidOtpError("Invalid OTP")
+
+    db_otp = await _validate_otp(
+        db,
+        user_id=user_id,
+        recipient=recipient,
+        otp_type=otp_type,
+        channel=channel,
+        code=code,
+        consume=False,
+    )
+
+    if not db_otp:
+        raise InvalidOtpError("Invalid OTP")
+
+    return db_otp
+
+
+async def validate_signup_service(
+    db: AsyncSession,
+    user_id: UUID,
+    channel: str,
+    recipient: str,
+    code: str,
+) -> M.User:
+
+    await _validate_otp(
+        db,
+        user_id=user_id,
+        recipient=recipient,
+        otp_type='signup',
+        channel=channel,
+        code=code,
+        consume=True,
+    )
+
+    db_user = await crud.get_user_by_id(db, user_id=user_id)
+    if not db_user:
+        raise UserNotFoundError()
+
+    db_user.verified = True
+
+    await db.commit()
+    await db.refresh(db_user)
+
+    return db_user
+
+
+async def validate_contact_change_service(
+    db: AsyncSession,
+    user_id: UUID,
+    recipient: str,
+    channel: str,
+    code: str,
+) -> M.User:
+
+    db_otp = await _validate_otp(
+        db,
+        user_id=user_id,
+        recipient=recipient,
+        otp_type=f'change_{channel}',
+        channel=channel,
+        code=code,
+        consume=True,
+    )
+
+    db_user = await crud.get_user_by_id(db, user_id=user_id)
+    if not db_user:
+        raise UserNotFoundError()
+
+    new_contact = db_otp.recipient
+    setattr(db_user, channel, new_contact)
+
+    await db.commit()
+    await db.refresh(db_user)
+
+    return db_user
+
+
+async def validate_password_change_service(
+    db: AsyncSession,
+    user_id: UUID | None,
+    recipient: str,
+    channel: str,
+    code: str,
+    new_password: str,
+) -> M.User:
+
+    if not user_id:
+        raise InvalidOtpError("Invalid OTP")
+
+    db_otp = await _validate_otp(
+        db,
+        user_id=user_id,
+        recipient=recipient,
+        otp_type='change_password',
+        channel=channel,
+        code=code,
+        consume=True,
+    )
+
+    if not db_otp.user_id:
+        raise UserNotFoundError()
+
+    user = await crud.get_user_by_id(db, db_otp.user_id)
+    if not user:
+        raise UserNotFoundError()
+
+    user.password_hash = hash_password(new_password)
+
+    await db.commit()
+    await db.refresh(user)
+
+    return user
