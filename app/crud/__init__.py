7d44f76d73ce0f11948bca9d023922a294e91a4f
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-11 10:36:48 +0200

    Suffix crud fonctions filenames

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-from app.crud.users import (
+from app.crud.user_crud import (
     create_user,
     update_user_by_id,
     get_user_by_id,
@@ -12,7 +12,7 @@ from app.crud.users import (
     update_user_password,
 )
 
-from app.crud.otp import (
+from app.crud.otp_crud import (
     create_otp,
     get_otp,
     increment_otp_attempts,
@@ -20,7 +20,7 @@ from app.crud.otp import (
     delete_expired_otp,
 )
 
-from app.crud.clients import (
+from app.crud.client_crud import (
     create_client,
     get_clients,
     get_client_by_client_id,
@@ -28,7 +28,7 @@ from app.crud.clients import (
     delete_client_by_id,
 )
 
-from app.crud.oauth_authorization_requests import (
+from app.crud.authorization_request_crud import (
     create_authorization_request,
     get_authorization_request_by_id,
     approve_authorization_request,
@@ -36,14 +36,14 @@ from app.crud.oauth_authorization_requests import (
     attach_user_to_authorization_request,
 )
 
-from app.crud.oauth_authorization_codes import (
+from app.crud.authorization_code_crud import (
     create_authorization_code,
     get_authorization_code,
     mark_authorization_code_as_used,
     delete_expired_authorization_codes,
 )
 
-from app.crud.sessions import (
+from app.crud.session_crud import (
     create_session,
     get_sessions_by_user_id,
     get_session_by_session_id,
