7e1022bc873e5b6ed136e77dda3bc7d1d8a489b5
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-11 17:16:38 +0200

    Define test api routes

---
+++ app/routes/health.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+from fastapi import APIRouter
+
+__all__ = [
+    'router',
+]
+
+router = APIRouter()
+
+
+@router.get('/health')
+async def health() -> dict:
+    return {'status': 'ok'}
