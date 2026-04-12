0fd9153b685f6727f7965d571eaf8c4ade1dc0a7
abdoulfataoh <abdoulfataoh@gmail.com>
2026-04-12 16:04:34 +0200

    Update API services interface

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -6,6 +6,7 @@ from app.services.oauth_service import (
     attach_user_to_request,
     get_authorization_request_by_id,
     approve_consent,
+    exchange_code_to_token,
 )
 
 from app.services.client_service import (
@@ -45,39 +46,40 @@ from app.services.session_service import (
 
 __all__ = [
     # oauth_service
-    "request_authorize",
-    "attach_user_to_request",
-    "get_authorization_request_by_id",
-    "approve_consent",
+    'request_authorize',
+    'attach_user_to_request',
+    'get_authorization_request_by_id',
+    'approve_consent',
+    'exchange_code_to_token',
 
     # client_service
-    "create_client",
-    "get_clients",
-    "get_client_by_client_id",
-    "get_client_by_id",
-    "delete_client_by_id",
+    'create_client',
+    'get_clients',
+    'get_client_by_client_id',
+    'get_client_by_id',
+    'delete_client_by_id',
 
     # user_verification_service
-    "send_otp",
-    "validate_signup",
-    "validate_contact_change",
-    "validate_password_change",
-    "resolve_otp_recipient",
-    "validate_otp_without_consume",
+    'send_otp',
+    'validate_signup',
+    'validate_contact_change',
+    'validate_password_change',
+    'resolve_otp_recipient',
+    'validate_otp_without_consume',
 
     # user_service
-    "signup",
-    "create_user",
-    "update_user_by_id",
-    "get_users",
-    "get_user_by_id",
-    "delete_user_by_id",
-    "silent_get_user_by_contact",
-    "update_user_password",
-    "authenticate_user",
+    'signup',
+    'create_user',
+    'update_user_by_id',
+    'get_users',
+    'get_user_by_id',
+    'delete_user_by_id',
+    'silent_get_user_by_contact',
+    'update_user_password',
+    'authenticate_user',
 
     # session_service
-    "create_session",
-    "get_my_sessions",
-    "logout_session",
+    'create_session',
+    'get_my_sessions',
+    'logout_session',
 ]
